8acfaa808203c57bea21bc616926bf98665d716b
purnesh42h <purnesh.dixit92@gmail.com>
2018-02-19 19:10:27 +0000

    Log in/Log out completed

--- lib/tracker_web/controllers/page_controller.ex
+++ lib/tracker_web/controllers/page_controller.ex
@@ -4,4 +4,10 @@ defmodule TrackerWeb.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+ 
+  def task(conn, _params) do
+    tasks = Tracker.Track.list_tasks()
+    changeset = Tracker.Track.change_task(%Tracker.Track.Task{})
+    render conn, "task.html", tasks: tasks, changeset: changeset
+  end
 end
