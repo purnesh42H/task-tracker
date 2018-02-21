71028499da1e2ff80db4173aeb12fdac6208d176
purnesh42h <purnesh.dixit92@gmail.com>
2018-02-21 02:28:50 +0000

    Added .sh scripts

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export PORT=5101
+export MIX_ENV=prod
+export GIT_PATH=/home/tracker/src/tracker
+
+mix deps.get
+(cd assets && npm install)
+(cd assets && ./node_modules/brunch/bin/brunch b -p)
+mix phx.digest
+mix release --env=prod
+
+mkdir -p ~/www
+mkdir -p ~/old
+
+NOW=`date +%s`
+if [ -d ~/www/tracker ]; then
+        echo mv ~/www/tracker ~/old/$NOW
+        mv ~/www/tracker ~/old/$NOW
+fi
+
+mkdir -p ~/www/tracker
+REL_TAR=~/src/tracker/_build/prod/rel/tracker/releases/0.0.1/tracker.tar.gz
+(cd ~/www/tracker && tar xzvf $REL_TAR)
+
+crontab - <<CRONTAB
+@reboot bash /home/tracker/src/tracker/start.sh
+CRONTAB
+
+#. start.sh
+
